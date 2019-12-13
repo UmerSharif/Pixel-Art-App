bf438d484a71374a8d0451831465575b2242810d
umer <umer4167@hotmail.com>
2019-12-13 16:13:55 +0200

    testing initial app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
+  console.log("hello ihjb");
   return (
     <div className="App">
-      
+      <p>hello</p>
     </div>
   );
 }
