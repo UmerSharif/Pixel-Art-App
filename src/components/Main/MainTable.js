d0cf1e3944196e06e59709ce31afebfea16b1d24
umer <umer4167@hotmail.com>
2019-12-23 20:08:57 +0200

    grid creation functionalities created

--- src/components/Main/MainTable.js
+++ src/components/Main/MainTable.js
@@ -1,9 +1,26 @@
 import React, { useState } from "react";
 import "./MainTable.css";
 export default function MainTable() {
+  // const TableRow = <tr></tr>;
+  //const TableColumn = <td></td>;
   // useState stuff
   const [row, setRow] = useState(10);
   const [col, setCol] = useState(10);
+  const [grid, setGrid] = useState([]);
+
+  const createGrid = () => {
+    const canvas = document.querySelector("#pixel-canvas");
+    canvas.innerHTML = "";
+    for (let i = 1; i <= row; i++) {
+      const tr = document.createElement("tr");
+      canvas.appendChild(tr);
+      for (let k = 1; k <= col; k++) {
+        const td = document.createElement("td");
+        tr.append(td);
+      }
+    }
+  };
+
   // useState stuff
   return (
     <>
@@ -49,13 +66,15 @@ export default function MainTable() {
 
         <div className="builder">
           <button type="button">Clear Grid</button>
-          <button type="button">Create Grid</button>
+          <button type="button" onClick={createGrid}>
+            Create Grid
+          </button>
         </div>
 
         <input type="color" />
       </div>
 
-      <table className="pixel-canvas"></table>
+      <table id="pixel-canvas"></table>
     </>
   );
 }
