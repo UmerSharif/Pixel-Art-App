65f0da392849eda18b26241add6f768f1ec126e0
umer <umer4167@hotmail.com>
2019-12-27 19:22:34 +0200

    background color to grid

--- src/components/Main/MainTable.js
+++ src/components/Main/MainTable.js
@@ -1,12 +1,15 @@
 import React, { useState } from "react";
 import "./MainTable.css";
+import { SketchPicker } from "react-color";
 export default function MainTable() {
-  // const TableRow = <tr></tr>;
-  //const TableColumn = <td></td>;
   // useState stuff
   const [row, setRow] = useState(10);
   const [col, setCol] = useState(10);
-  const [grid, setGrid] = useState([]);
+  const [background, setBackground] = useState("#fff");
+
+  const handleBackgroundColor = color => {
+    setBackground(color.hex);
+  };
 
   const createGrid = () => {
     const canvas = document.querySelector("#pixel-canvas");
@@ -25,11 +28,7 @@ export default function MainTable() {
   return (
     <>
       <div className="controls">
-        <button type="button" id="reset">
-          Reset
-        </button>
         <label className="builder">
-         
           <input
             type="number"
             name="Trow"
@@ -41,7 +40,6 @@ export default function MainTable() {
         </label>
 
         <label className="builder">
-        
           <input
             type="number"
             name="Tcol"
@@ -53,16 +51,20 @@ export default function MainTable() {
         </label>
 
         <div className="builder">
-          <button type="button">Clear Grid</button>
           <button type="button" onClick={createGrid}>
             Create Grid
           </button>
+          <button type="button">Clear Grid</button>
+          <button type="button" id="reset">
+            Reset
+          </button>
         </div>
-
-        <input type="color" />
       </div>
-
-      <table id="pixel-canvas"></table>
+      <SketchPicker
+        color={background}
+        onChangeComplete={handleBackgroundColor}
+      />
+      <table id="pixel-canvas" style={{ backgroundColor: background }}></table>
     </>
   );
 }
