5cea3ed485e01ff07b910b5e143302c1a904eeec
umer <umer4167@hotmail.com>
2019-12-29 01:36:52 +0200

    setting footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import MainTable from "./components/Main/MainTable";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <div className="App">
       <MainTable />
+      <Footer />
     </div>
   );
 }
