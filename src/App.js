16e35f3bdaa4afca5b596d221131ee4dc27649b5
umer <umer4167@hotmail.com>
2019-12-28 20:24:19 +0200

    fixing height problem

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import "./App.css";
 import MainTable from "./components/Main/MainTable";
 
 function App() {
-  console.log("hello ihjb");
   return (
     <div className="App">
       <MainTable />
