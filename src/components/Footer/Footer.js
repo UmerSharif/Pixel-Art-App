cc84e3a815546dcc48062b074d93cd4c0564a132
umer <umer4167@hotmail.com>
2019-12-29 20:26:58 +0200

    footer changes

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Footer.css";
+export default function Footer() {
+  return (
+    <div className="footer">
+      <div className="overlay">
+        <h2>Design and Created By @ Umer</h2>
+      </div>
+    </div>
+  );
+}
